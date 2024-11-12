599d694f8bf95ab9211c4f167231c57cf054497e
Svetlana Strogaleva <lana.strogaleva@gmail.com>
2024-11-12 11:37:08 +0300

    Add oop.abstractandinterface

--- src/main/java/org/example/oop/abstractandinterface/AbstractClassExample.java
+++ src/main/java/org/example/oop/abstractandinterface/AbstractClassExample.java
@@ -19,4 +19,46 @@ package org.example.oop.abstractandinterface;
  * </ul>
  */
 abstract class ShapeForAbstract {
+    /**
+     * Абстрактный метод для расчета площади фигуры.
+     */
+    public abstract double calculateArea();
+
+    /**
+     * Абстрактный метод для расчета периметра фигуры.
+     */
+    public abstract double calculatePerimeter();
+
+    /**
+     * Метод для вывода канонического имени класса объекта.
+     */
+    public void printObjectClass() {
+        System.out.println("Класс объекта: " + this.getClass().getCanonicalName());
+    }
+
+    /**
+     *  класс Circle, который наследует ShapeForAbstract и реализует методы calculateArea и calculatePerimeter.
+     */
+    public static class Circle extends ShapeForAbstract{
+        private double radius;
+
+        public Circle(double radius) {
+            this.radius = radius;
+        }
+        @Override
+        public double calculateArea() {
+            return Math.round(Math.PI*radius*radius);
+        }
+        @Override
+        public double calculatePerimeter() {
+            return Math.round(2 * Math.PI * radius);
+        }
+    }
+
+    public static void main(String[] args) {
+        ShapeForAbstract circle = new Circle(5.0);
+        System.out.println("Периметр: " + circle.calculatePerimeter());
+        System.out.println("Площадь: " + circle.calculateArea());
+        circle.printObjectClass();
+    }
 }
